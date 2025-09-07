b2c03579b8db1bc4b3dde69c98c82bab50ce0be2
Vithurshan2002 <sangervithu2002@gmail.com>
2025-09-07 22:09:11 +0530

    privateroute functionality added

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { setuser } from "../slices/userslice";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+        const dispatch=useDispatch();
+        const navigate=useNavigate();
+    const login=()=>{
+        dispatch(setuser({email:"ultra@gamil.com",password:"0000"}))
+        navigate("/");
+    }
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <div>Login</div>
+      <button onClick={login}className="px-2 py-1 bg-green-700 cursor-pointer transition-all hover:bg-green-400 rounded-full font-bold mt-5">Login</button>
+    </>
+  );
+};
 
-export default Login
+export default Login;
